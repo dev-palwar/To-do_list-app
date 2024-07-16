4fddbe7f369d538dc2c5240b88b40e3c00f1115c
Dev Palwar <devpalwar06@gmail.com>
2024-07-16 16:13:36 +0530

    fixing vercel deployment

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const todoItems = "todoItems";
 document.addEventListener("DOMContentLoaded", () => {
     const inputBox = document.getElementById("text");
